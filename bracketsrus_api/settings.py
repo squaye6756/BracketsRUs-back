16fe1c1f87ddc075fdc603464edf3173ade9b88b
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-11-17 16:00:08 -0500

    doing what heroku tells us

--- bracketsrus_api/settings.py
+++ bracketsrus_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://bracketsrus.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'bracketsrus.herokuapp.com']
 
 
 # Application definition

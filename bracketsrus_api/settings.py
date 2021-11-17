9080e88e514e15126188876e88c388b8099ebf60
Samuel Quaye <quayes955@gmail.com>
2021-11-17 11:58:23 -0500

    removed secret key from settings.py

--- bracketsrus_api/settings.py
+++ bracketsrus_api/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--c9kiimlotuq(vq3-da6-#)k@5b@+g_#uo%ao9&1nc)*r==!$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

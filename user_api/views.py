d22b42183fb90831e6f619c29bf6d22af9ba3cb6
Samuel Quaye <quayes955@gmail.com>
2021-11-18 11:36:36 -0500

    backend returns dict with error message values upon unsuccesful login attempts

--- user_api/views.py
+++ user_api/views.py
@@ -32,11 +32,14 @@ def check_login(request):
         jsonRequest = json.loads(request.body) #make the request JSON format
         username = jsonRequest['username'] #get the username from the request
         password = jsonRequest['password'] #get the password from the request
-        if User.objects.get(username=username): #see if username exists in db
-            user = User.objects.get(username=username)  #find user object with matching username
-            if check_password(password, user.password): #check if passwords match
-                return JsonResponse({'id': user.id, 'username': user.username}) #if passwords match, return a user dict
-            else: #passwords don't match so return empty dict
+        try:
+            if User.objects.get(username=username): #see if username exists in db
+                user = User.objects.get(username=username)  #find user object with matching username
+                if check_password(password, user.password): #check if passwords match
+                    return JsonResponse({'id': user.id, 'username': user.username}) #if passwords match, return a user dict
+                else: #passwords don't match
+                    return JsonResponse({"error":"Incorrect Password"})
+            else: #if username doesn't exist in db, return empty dict
                 return JsonResponse({})
-        else: #if username doesn't exist in db, return empty dict
-            return JsonResponse({})
+        except:
+            return JsonResponse({"error":"No Matching Username"})
